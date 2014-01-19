79d301c940a0ce1ed55dad0c87d1271a92d8d3aa
Bradley Shively <bshively@gmail.com>
2014-01-19 04:22:36 -0500

    Added validations for user.name, pin.description, and pin.image

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -2,4 +2,7 @@ class Pin < ActiveRecord::Base
   belongs_to :user
 
   has_attached_file :image, :styles => { :medium => "300x300", :thumb => "100x100"}
+
+  validates :image, presence: true
+  validates :description, presence: true, length: { maximum: 140, too_long: "is too long. %{count} characters is the limit."}
 end
